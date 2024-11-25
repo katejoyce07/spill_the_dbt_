c2f235e241c4e442a531fffc9a55008ea337cd27
katejoyce07 <kate.joyce@theinformationlab.co.uk>
2024-11-25 10:31:23 +0000

    Commit message

---
+++ challenge_01/models/lego/unique_parts.sql
@@ -0,0 +1,9 @@
+-- This model selects unique part numbers that appear only once in the inventory.
+SELECT 
+  P.part_num
+FROM parts as P
+INNER JOIN inventory_parts as IP on P.part_num = IP.part_num
+INNER JOIN inventories as I on I.id = IP.inventory_id
+INNER JOIN sets as S on S.set_num = I.set_num
+GROUP BY P.part_num
+HAVING COUNT(*) = 1
